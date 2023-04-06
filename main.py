f89c4527dc10cf1b62a1dbb473cfa8c922ab0fc0
PromiMojumder38 <74852911+PromiMojumder38@users.noreply.github.com>
2023-04-06 10:59:00 +0600

    checking duplicated and null values

--- main.py
+++ main.py
@@ -10,7 +10,24 @@ data = pd.read_csv('IMDB-Movie-Data.csv')
 #print(data.head(10))
 #print(data.tail(10))
 #print(data.shape[0])
-print(data.info())
+#print(data.info())
+#print(data.isnull().values.any())
+#print(data.isnull())
+#print(data.isnull().sum())
+
+"""Checking null values 
+print(sns.heatmap(data.isnull()))
+plt.show()"""
+
+"""dropping null values
+data = data.dropna(axis=0)
+print(sns.heatmap(data.isnull()))
+plt.show()"""
+
+"""checking duplicated data
+print(data.duplicated().any())"""
+
+
 
 
 
